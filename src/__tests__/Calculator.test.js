ac0768bdb955aee75a53e220b7337f48ad0e491d
Luis Muñoz <l.munoz.m@outlook.com>
2022-12-08 13:23:10 -0300

    Add user interaction test

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,4 +1,9 @@
+import React from 'react';
 import renderer from 'react-test-renderer';
+import {
+  render, fireEvent, screen,
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Calculator from '../components/Calculator';
 
 it('renders correctly', () => {
@@ -6,4 +11,11 @@ it('renders correctly', () => {
     .create(<Calculator />)
     .toJSON();
   expect(calculator).toMatchSnapshot();
+});
+
+test('user interaction with calc', async () => {
+  render(<Calculator url="/calculator" />);
+  fireEvent.click(screen.getByText('8'));
+  fireEvent.click(screen.getByText('7'));
+  expect(screen.getByText('87')).toBeInTheDocument();
 });
